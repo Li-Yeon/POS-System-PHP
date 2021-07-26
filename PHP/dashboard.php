9b01e4c9128315d5be22ac89aa4f274ffb757e6c
MekaXV <48040967+MekaXV@users.noreply.github.com>
2021-07-26 18:49:44 +0800

    Hugee dashboard changes

--- PHP/dashboard.php
+++ PHP/dashboard.php
@@ -16,4 +16,32 @@ $customerQuery = "SELECT COUNT(*) AS 'TotalCustomer' FROM customers";
 $cstResult = mysqli_query($conn, $customerQuery);
 $totalCst = mysqli_fetch_assoc($cstResult);
 $totalCst = $totalCst['TotalCustomer'];
+
+//Get Total Stocks
+$query = "SELECT SUM(Stock) AS 'Stock' from products";
+$StockInRes = mysqli_query($conn, $query);
+$StockInVal = mysqli_fetch_assoc($StockInRes);
+$StockInSum = (int)$StockInVal['Stock'];
+$totalSum = $StockInSum;
+
+$getSumQuery = "SELECT IFNULL( (SELECT SUM(Total) As 'Total' from transaction) ,'Null') As 'Total'";
+    $getSumSqli = mysqli_query($conn, $getSumQuery);
+    $getSum = mysqli_fetch_assoc($getSumSqli);
+    $getSum = $getSum['Total'];
+    if ($getSum == 'Null')
+    {
+        $getSum = "0";
+    }
+
+//Top 5 Selling Products
+$transQuery = "SELECT Product_Name, Price, SUM(Quantity) As TotalSold FROM transaction GROUP By Product_Name ORDER BY TotalSold DESC LIMIT 0,5";
+$transTable = mysqli_query($conn, $transQuery);
+
+//Top Staff
+$staffQuery = "SELECT Issued, SUM(Total) As TotalSold FROM transaction GROUP By Issued ORDER BY TotalSold DESC LIMIT 0,5";
+$staffTable = mysqli_query($conn, $staffQuery);
+
+//Recent Stock In
+$stockInQuery = "SELECT Product_Name, Stock, Supplier FROM stock_in ORDER BY No DESC LIMIT 0,5";
+$stockInTable = mysqli_query($conn, $stockInQuery);
 ?>
